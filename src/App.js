338165ddbeb7488cde0b111b432fb7a155dcc40d
Natalia <nataliamirandac@hotmail.com>
2020-05-12 18:12:17 -0400

    Created the connect and mapStateToProps in the App and passed props replacing what it needed - displayed the data on the browser

--- src/App.js
+++ src/App.js
@@ -4,20 +4,34 @@ import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
+import { connect } from "react-redux";
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+//mapStateToProps tell to connect which
+//pieces of our state we want to bring in to this component.
+//This function takes in state as a parameter, then returns an object where the properties can be passed to props, and the values are retrieved from the store for our component.
+
+const mapStateToProps = (state) => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    features: state.features,
+    additionalFeatures: state.additionalFeatures,
+  };
+};
+
+export default connect(mapStateToProps, {})(App);
