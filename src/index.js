c69e9a465e4e85e774bec10a484f674391d6a0c5
Natalia <nataliamirandac@hotmail.com>
2020-05-12 17:57:35 -0400

    Created store and reducerFile - wrapped Provider to App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
+import { Provider } from "react-redux";
+import { reducerFile } from "./reducers/reducerFile";
+import { createStore } from "redux";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = createStore(reducerFile);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
